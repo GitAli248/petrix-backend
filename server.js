f82253d2340c1c8ce58bf2464c349933e7678ec8
ali.dev <syedalimohammd101@gmail.com>
2026-05-29 16:35:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 app.use('/auth', authRoutes);
-app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => console.log('Server running at http://localhost:' + port));
